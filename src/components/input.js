40ae306911e86528a135f2db9d6325043603a2f7
Kilian Rullkötter <kilian@kjubo.com>
2020-05-21 20:49:26 +0200

    added Input Field styling

---
+++ src/components/input.js
@@ -0,0 +1,35 @@
+import React from "react"
+import {ThemeProvider, StylesProvider} from '@material-ui/core/styles';
+import {Input} from '@material-ui/core';
+import theme from '../utils/theme';
+import styled from 'styled-components';
+
+const MyInput = styled(Input)`
+    color: white;
+    font-size: 20px;
+    line-height: 32px;
+    
+    &.MuiInput-underline:before {
+        border-bottom: 1px solid rgba(255, 255, 255, 0.8); 
+    }
+    &.MuiInput-underline:hover:not(.Mui-disabled):before{
+        border-bottom: 1px solid white;
+    }
+    &.MuiInput-underline:after {
+        border-bottom: 2px solid white;
+    }
+`
+
+const StyledInput = ({ children, ...props }) => {
+    return(
+        <ThemeProvider theme={theme}>
+            <StylesProvider injectFirst>
+                <MyInput {...props}>
+                    {children}
+                </MyInput>
+            </StylesProvider>
+        </ThemeProvider>
+    )
+}
+
+export default StyledInput
--- src/sections/newslettersection.js
+++ src/sections/newslettersection.js
@@ -2,7 +2,14 @@ import React from "react"
 import styled from "styled-components"
 import Section from "../components/section"
 import Container from '@material-ui/core/Container';
+import StyledInput from "../components/input";
 
+//this background should ideally be included in the Section component in a way that
+//the Section component has 3 different background themes.
+const Background = styled.div`
+    background-color: ${props => props.theme.palette.primary.main};
+    color: white;
+`
 const StyledContainer = styled(Container)`
     text-align: center;
     padding-top: 64px;
@@ -12,9 +19,13 @@ const StyledContainer = styled(Container)`
 const NewsletterSection = () => {
     return(
         <Section>
+            <Background>
             <StyledContainer maxWidth="md">
                 <h3>Get the check list “From Idea to Impact” for free.</h3>
+                <StyledInput placeholder="Name"/>
+                <StyledInput placeholder="E-Mail"/>
             </StyledContainer>
+            </Background>
         </Section>
     )   
 }
